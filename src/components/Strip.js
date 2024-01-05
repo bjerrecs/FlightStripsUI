907548a06f26813e55b4e2fb1031ebf36cb2e1ff
Simon Bjerre <simon.bjerre@team.blue>
2024-01-05 11:30:34 +0100

    strip format styleing

--- src/components/Strip.js
+++ src/components/Strip.js
@@ -2,22 +2,22 @@ import React from "react";
 
 export const Strip = (props) => {
   return (
-    <div className="w-[90%] h-16 bg-[#BEF5EF] border-4 flex items-center font-bold ">
-        <div className="border-r-2 border-l-4 border-t-4 border-b-4 border-[#85B4AF] w-48 pr-8 h-full flex items-center pl-2 text-2xl">
+    <div className="w-fit h-16 bg-[#BEF5EF] border-l-4 border-r-4 border-t-2 border-b-2 mb-1 flex items-center">
+        <div className="border-r-2 border-l-2 border-t-2 border-b-2 border-[#85B4AF] w-36 h-full flex items-center pl-2 text-xl font-bold">
             {props.callsign}
         </div>
-        <div className="border-r-2 border-l-2 border-t-4 border-b-4 border-[#85B4AF] w-fit pl-4 pr-4 h-full flex flex-col  items-center justify-center text-center">
+        <div className="border-r-1 border-l-1 border-t-2 border-b-2 border-[#85B4AF] w-16 pl-4 pr-4 h-full flex flex-col  items-center justify-center text-center">
             <span className="font-extrabold">{props.ades}</span><span className="font-extrabold">{props.stand}</span>
         </div>
-        <div className="border-r-2 border-l-2 border-t-4 border-b-4 border-[#85B4AF] w-32 h-full flex items-top justify-between text-center pl-2 pr-2">
+        <div className="border-r-1 border-l-1 border-t-2 border-b-2 border-[#85B4AF] w-32 h-full flex items-top justify-between text-center pl-2 pr-2">
             <div>EOBT</div> <div>{props.eobt}</div>
         </div>
-        <div className="border-r-2 border-l-2 border-t-4 border-b-4 border-[#85B4AF] w-full h-full flex flex-col">
-            <span className="border-[#85B4AF] border-b-2 border-r-2 text-left flex items-center w-full h-full">
-            TSAT:{props.tsat}
+        <div className="border-r-2 border-l-1 border-t-2 border-b-2 border-[#85B4AF] w-32 h-full flex flex-col">
+            <span className="border-[#85B4AF] border-b-1 border-r-1 text-left flex items-center w-full h-full pl-2">
+            TSAT: {props.tsat}
             </span>
-            <div className="border-[#85B4AF] border-t-2 border-r-2 text-left flex items-center w-full h-full">
-            CTOT:{props.ctot}
+            <div className="border-[#85B4AF] border-t-1 border-r-1 text-left flex items-center w-full h-full pl-2">
+            CTOT: {props.ctot}
             </div>
         </div>
     </div>
--- src/views/Delivery.js
+++ src/views/Delivery.js
@@ -10,47 +10,49 @@ export const Delivery = () => {
   return (
     <div className="h-screen">
       <div className="bg-[#A9A9A9] w-screen h-full flex justify-center justify-items-center shrink">
-        <div className="bg-[#555355] w-full h-auto ml-0 mr-2">
-          <Header headerName="OTHERS" buttons={<FindFlight/>}/>
-          <Strip callsign="VKG1332/t" ades="LGPR" stand="D3" eobt="1350" tsat="1400" ctot="1400"/>
+        <div className="bg-[#555355] w-full h-auto border-r-4 border-[#a9a9a9]">
+          <Header headerName="OTHERS" buttons={<FindFlight />} />
+          <Strip callsign="VKG1332/t" ades="LGPR" stand="D3" eobt="1350" tsat="1400" ctot="1400" />
           <PushbackModal />
         </div>
-        <div className="bg-[#555355] w-full h-auto ml-1 mr-1.5">
+        <div className="bg-[#555355] w-full h-auto border-l-2 border-r-2 border-[#a9a9a9]">
           <Header headerName="SAS" />
           <div className="h-3/5 overflow-auto overflow-x-hidden">
-            <Strip callsign="SAS42T" ades="EKYT" stand="B6" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS7736" ades="LEPA" stand="B4" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAD43G" ades="ENGM" stand="B8" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS418" ades="ESSA" stand="B7" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS031" ades="LHBP" stand="B9" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS446Y" ades="EGKK" stand="B16" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS9909" ades="ENBR" stand="A12" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS23" ades="EKBI" stand="A14" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS928" ades="EKVG" stand="A11" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS887" ades="KBOS" stand="B10" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS42T" ades="EKYT" stand="B6" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAS7736" ades="LEPA" stand="B4" eobt="1350" tsat="1400" ctot="1400"/>
-            <Strip callsign="SAD43G" ades="ENGM" stand="B8" eobt="1350" tsat="1400" ctot="1400"/>
+            <Strip callsign="SAS42T" ades="EKYT" stand="B6" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS7736" ades="LEPA" stand="B4" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAD43G" ades="ENGM" stand="B8" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS418" ades="ESSA" stand="B7" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS031" ades="LHBP" stand="B9" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS446Y" ades="EGKK" stand="B16" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS9909" ades="ENBR" stand="A12" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS23" ades="EKBI" stand="A14" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS928" ades="EKVG" stand="A11" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS887" ades="KBOS" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS42T" ades="EKYT" stand="B6" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="SAS7736" ades="LEPA" stand="B4" eobt="1350" tsat="1400" ctot="1400" />
           </div>
           <Header headerName="NORWEGIAN" />
-          <div className="overflow-auto overflow-x-hidden">
-          <Strip callsign="NZS38F" ades="KBOS" stand="B10" eobt="1350" tsat="1400" ctot="1400"/>
-          <Strip callsign="NAX948" ades="ENGM" stand="B10" eobt="1350" tsat="1400" ctot="1400"/>
-          <Strip callsign="NOZ662" ades="EKYT" stand="B10" eobt="1350" tsat="1400" ctot="1400"/>
-          <Strip callsign="NZS3096" ades="EKBI" stand="B10" eobt="1350" tsat="1400" ctot="1400"/>
-          <Strip callsign="NZS38F" ades="EGKK" stand="B10" eobt="1350" tsat="1400" ctot="1400"/>
-          <Strip callsign="NZS342" ades="ESSA" stand="B10" eobt="1350" tsat="1400" ctot="1400"/>
-
+          <div className="h-2/5 overflow-auto overflow-x-hidden">
+            <Strip callsign="NZS38F" ades="KBOS" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="NAX948" ades="ENGM" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="NOZ662" ades="EKYT" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="NZS3096" ades="EKBI" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="NZS38F" ades="EGKK" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="NZS342" ades="ESSA" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+            <Strip callsign="NZS342" ades="ESSA" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
           </div>
         </div>
-        <div className="bg-[#555355] w-full h-auto ml-1.5 mr-1">
+        <div className="bg-[#555355] w-full h-auto border-l-2 border-r-2 border-[#a9a9a9]">
           <Header headerName="CLEARED" />
-          <div className="h-2/4"></div>
-          <Header headerName="MESSAGES" TypeMSG buttons={<MSGModal/>}/>
+          <div className="h-2/4">
+            <Strip callsign="NZS342" ades="ESSA" stand="B10" eobt="1350" tsat="1400" ctot="1400" />
+          </div>
+          <Header headerName="MESSAGES" TypeMSG buttons={<MSGModal />} />
         </div>
-        <div className="bg-[#555355] w-full h-auto ml-2 mr-0">
+        <div className="bg-[#555355] w-full h-auto border-l-2 border-[#a9a9a9]">
           <Header headerName="PUSHBACK" />
-          <div className="h-1/3"></div><Header headerName="TWY DEP" />
+          <div className="h-1/3">
+            <Strip callsign="NZS342" ades="ESSA" stand="B10" eobt="1350" tsat="1400" ctot="1400" /></div><Header headerName="TWY DEP" />
         </div>
       </div>
       <Commandstrip />
